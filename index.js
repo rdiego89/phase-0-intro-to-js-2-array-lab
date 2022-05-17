4d461ad694f7f65aa5275b4fc8eb5b0a6ee8c1d9
Rebecca Diego <rebecca.diego89@gmail.com>
2022-05-16 14:53:48 -1000

    commit tree 2c67968b7babb775b2f3af887c434e8599d89ca0

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+// destructivelyAppendCat = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(cat) {
+    cats.push(cat);
+
+}
+
+function destructivelyPrependCat(john) {
+    cats.unshift(john);
+
+}
+
+function destructivelyRemoveLastCat(garfield) {
+    cats.pop(garfield);
+
+}
+
+function destructivelyRemoveFirstCat(milo) {
+    cats.shift(milo);
+}
+
+function appendCat(name) {
+    // console.log('cat;', name)
+    const copyOfCats = [...cats, name];
+    return (copyOfCats)
+}
+
+function prependCat(john) {
+    const copyOfCats = [john, ...cats];
+    return (copyOfCats)
+}
+
+function removeLastCat(name) {
+const copyOfCats = [...cats]
+copyOfCats.pop()
+return (copyOfCats)
+}
+
+function removeFirstCat(name) {
+    const copyOfCats = [...cats]
+    copyOfCats.shift()
+    return(copyOfCats)
+}
+
